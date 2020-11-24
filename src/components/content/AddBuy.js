c18e1d24b584dc2c86d049e8ee7e2c2d2686b1cb
Carlos Vargas <carlosospino13@gmail.com>
2020-11-24 11:59:36 -0500

    Validación del formulario Registrar compra hecho.

--- src/components/content/AddBuy.js
+++ src/components/content/AddBuy.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Swal from "sweetalert2";
 import { useForm } from "../../hooks/useForm";
 import { useFormArray } from "../../hooks/useFormArray";
 
 export const AddBuy = () => {
 	const products = useSelector(state => state.products);
 
-	const [formValues, handleInputChange] = useForm({
+	const [formValues, handleInputChange, errores, validarForm, formEmpty] = useForm({
 			documento: "",
 			nombres: "",
 			apellidos: "",
@@ -16,7 +17,7 @@ export const AddBuy = () => {
 		{ documento, nombres, apellidos, telefono, email } = formValues;
 
 	const [productsBuy, addProduct, updateProducts, deleteProducts] = useFormArray([
-		{	
+		{
 			id: new Date().getTime(),
 			modelo: "Redmi Airdots",
 			unidad: 1
@@ -26,6 +27,17 @@ export const AddBuy = () => {
 	const handleSubmit = e => {
 		e.preventDefault();
 
+		if(!validarForm()) {
+			Swal.fire('Error', 'Completa los campos correctamente.', 'error')
+			return;
+		} 
+
+		if(formEmpty()) {
+			Swal.fire('Error', 'Completa los campos solicitados.', 'error');
+			return;
+		} 
+
+		console.log(formValues);
 		console.log(productsBuy);
 	};
 
@@ -35,55 +47,74 @@ export const AddBuy = () => {
 
 			<form onSubmit={handleSubmit} className="buy">
 				<label>
-					Documento
+					Documento <b>(*)</b>
 					<input
 						type="text"
 						name="documento"
 						onChange={handleInputChange}
 						value={documento}
 						className="txtDocumento"
+						autoComplete="off"
+						required
 					/>
 				</label>
+				{errores.documento && <div className="error">El documento no es válido.</div>}
+
 				<label>
-					Nombres
+					Nombres <b>(*)</b>
 					<input
 						type="text"
 						name="nombres"
 						onChange={handleInputChange}
 						value={nombres}
 						className="txtNombres"
+						autoComplete="off"
+						required
 					/>
 				</label>
+				{errores.nombres && <div className="error">Los nombres son requeridos.</div>}
+
 				<label>
-					Apellidos
+					Apellidos <b>(*)</b>
 					<input
 						type="text"
 						name="apellidos"
 						onChange={handleInputChange}
 						value={apellidos}
 						className="txtApellidos"
+						autoComplete="off"
+						required
 					/>
 				</label>
+				{errores.apellidos && <div className="error">Los apellidos son requeridos.</div>}
+
 				<label>
-					Teléfono
+					Teléfono <b>(*)</b>
 					<input
 						type="tel"
 						name="telefono"
 						onChange={handleInputChange}
 						value={telefono}
 						className="txtTelefono"
+						autoComplete="off"
+						required
 					/>
 				</label>
+				{errores.telefono && <div className="error">El teléfono no es válido.</div>}
+
 				<label>
-					Email
+					Email <b>(*)</b>
 					<input
 						type="email"
 						name="email"
 						onChange={handleInputChange}
 						value={email}
 						className="txtEmail"
+						autoComplete="off"
+						required
 					/>
 				</label>
+				{errores.email && <div className="error">El email no es válido.</div>}
 
 				<div className="buy--products">
 					{productsBuy.length > 0 &&
@@ -92,9 +123,7 @@ export const AddBuy = () => {
 								<div className="buy--product-body">
 									<label>
 										Nombre del producto
-										<select name="modelo" 
-										data-id={productBuy.id}
-										onChange={updateProducts}>
+										<select name="modelo" data-id={productBuy.id} onChange={updateProducts}>
 											{products.length > 0 &&
 												products.map(product => (
 													<option key={product.id} value={product.modelo}>
@@ -107,6 +136,8 @@ export const AddBuy = () => {
 										Unidades compradas
 										<input
 											name="unidad"
+											min="1"
+											max="5"
 											data-id={productBuy.id}
 											onChange={updateProducts}
 											type="number"
@@ -115,19 +146,23 @@ export const AddBuy = () => {
 										/>
 									</label>
 								</div>
-								{i > 0 && 
-									<button 
-									onClick={deleteProducts}
-									data-id={productBuy.id}
-									className="btn btn--danger">Borrar producto</button>
-								}
-								
+								{i > 0 && (
+									<button
+										onClick={deleteProducts}
+										data-id={productBuy.id}
+										className="btn btn--danger"
+									>
+										Borrar producto
+									</button>
+								)}
 							</div>
 						))}
 				</div>
 
 				<div className="buy--actions">
-					<button type="button" onClick={addProduct} className="btn btn--secondary">Agregar producto</button>
+					<button type="button" onClick={addProduct} className="btn btn--secondary">
+						Agregar producto
+					</button>
 					<button type="submit" className="btn btn--cta">
 						Ingresar compra
 					</button>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -4,11 +4,89 @@ export const useForm = (initialState = {}) => {
 
     const [values, setValues] = useState(initialState);
 
+    const [errores, setErrores] = useState({
+		documento: false,
+		nombres: false,
+		apellidos: false,
+		telefono: false,
+		email: false
+	});
+
     const reset = () => {
         setValues(initialState);
     }
 
+    const formEmpty = () => {
+        const {documento, nombres, apellidos, telefono, email} = values;
+
+        if(documento.length === 0) return true;
+        if(nombres.length === 0) return true;
+        if(apellidos.length === 0) return true;
+        if(telefono.length === 0) return true;
+        if(email.length === 0) return true;
+
+        return false;
+    }
+
+    const validarForm = () => {
+        const {documento, nombres, apellidos, telefono, email} = errores;
+
+        if(documento) return false;
+        if(nombres) return false;
+        if(apellidos) return false;
+        if(telefono) return false;
+        if(email) return false;
+
+        return true;
+    }
+
     const handleInputChange = ({target}) => {
+
+        let bandera = true;
+
+        if(target.value === '') {
+            setErrores({
+                ...errores,
+                [target.name]: true
+            })
+        } else {
+
+            if(target.name === 'documento' || target.name === 'telefono') {
+                
+                const regExp = /^\d+$/;
+
+                if(!regExp.test(target.value)) return;
+
+                if(target.name === 'documento' && (target.value.length < 10 || target.value.length > 11)) bandera = false;
+
+                if(target.name === 'telefono' && (target.value.length < 7 || target.value.length > 10)) bandera = false;
+            } 
+
+            if(target.name === 'nombres' || target.name === 'apellidos') {
+                
+                const regExp = /^[a-zA-ZÁÄÉËÍÏÓÖÚÜáäéëíïóöúü\s]+$/i;
+
+                if(!regExp.test(target.value)) return;
+
+            } 
+
+            if(target.name === 'email') {
+                const regExp = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+                if(!regExp.test(target.value)) bandera = false;
+            }
+
+            if(bandera) setErrores({
+                ...errores,
+                [target.name]: false
+            }) 
+            else setErrores({
+                ...errores,
+                [target.name]: true
+            })
+            
+        }
+
         setValues({
             ...values,
             [target.name]: target.value
@@ -18,8 +96,10 @@ export const useForm = (initialState = {}) => {
     return [
         values,
         handleInputChange,
+        errores,
+        validarForm,
+        formEmpty,
         reset,
-        setValues
     ]
 
 }
